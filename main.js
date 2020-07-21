44d4076978c79c8d90ab5e5440a77b0dfda76440
MahdiHat791 <67454600+MahdiHat791@users.noreply.github.com>
2020-07-21 14:45:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+
+function submits()
+{
+  url = document.getElementById('url').value;
+  document.getElementById("img").src = url;
+}      
+
+  console.log('ml5 version:', ml5.version);
+  
+classifier = ml5.imageClassifier('MobileNet',modelLoaded);
+
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+      
+  function check()
+  {
+    img = document.getElementById('img');
+    classifier.classify(img, gotResult);
+  }
+
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    document.getElementById("object_name").innerHTML = results[0].label;
+  }
+}
